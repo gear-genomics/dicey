1287e09c8141a6c1b020f65a41c2a458c90d9452
tobiasrausch <rauschtobi@gmail.com>
2019-01-21 09:35:35 +0100

    json2txt

--- scripts/json2txt.py
+++ scripts/json2txt.py
@@ -9,7 +9,6 @@ for line in fileinput.input():
     df = json.loads(line)
     for hit in df['data']:
         print(">", hit['chr'], ":", hit['start'], "-", hit['end'], " - Score:", hit['score'], sep="")
-        {u'queryalign': u'-ACTGATGTTAGTAATG--', u'end': 18800, u'refalign': u'TAGTGATGTTAGTAATGCA', u'start': 18785, u'chr': u'KN150570.1', u'score': 14, u'strand': u'-'}
         print(hit['queryalign'])
         print(hit['refalign'])
         print()
