2084dd629f7f785b7040930381561a6e400a4141
Tobias Rausch <rauschtobi@gmail.com>
2022-10-20 15:33:52 +0200

    GPLv3

--- src/version.h
+++ src/version.h
@@ -13,7 +13,7 @@ namespace dicey
       std::cout << "**********************************************************************" << std::endl;
       std::cout << "Program: Dicey" << std::endl;
       std::cout << "This is free software, and you are welcome to redistribute it under" << std::endl;
-      std::cout << "certain conditions (BSD License); for license details use '-l'." << std::endl;
+      std::cout << "certain conditions (GPL Version 3); for license details use '-l'." << std::endl;
       std::cout << "This program comes with ABSOLUTELY NO WARRANTY; for details use '-w'." << std::endl;
       std::cout <<  std::endl;
       std::cout <<  title << " (Version: " << diceyVersionNumber << ")" << std::endl;
@@ -23,33 +23,29 @@ namespace dicey
     }
 
   inline
-    void displayWarranty()
-    {
-      std::cout << "THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, ";
-      std::cout << "THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR ";
-      std::cout << "CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, ";
-      std::cout << "PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, ";
-      std::cout << "WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ";
-      std::cout << "ADVISED OF THE POSSIBILITY OF SUCH DAMAGE." << std::endl;
-      std::cout << std::endl;
-    }
+  void displayWarranty() {
+    std::cout << "Copyright (C) 2019 European Molecular Biology Laboratory (EMBL)" << std::endl;
+    std::cout << std::endl;
+    std::cout << "This program is free software: you can redistribute it and/or modify" << std::endl;
+    std::cout << "it under the terms of the GNU General Public License as published by" << std::endl;
+    std::cout << "the Free Software Foundation, either version 3 of the License, or" << std::endl;
+    std::cout << "(at your option) any later version." << std::endl;
+    std::cout << std::endl;
+    std::cout << "This program is distributed in the hope that it will be useful," << std::endl;
+    std::cout << "but WITHOUT ANY WARRANTY; without even the implied warranty of" << std::endl;
+    std::cout << "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the" << std::endl;
+    std::cout << "GNU General Public License for more details." << std::endl;
+    std::cout	<< std::endl;
+    std::cout << "You should have received a copy of the GNU General Public License" << std::endl;
+    std::cout << "along with this program.  If not, see https://www.gnu.org/licenses/" << std::endl;
+  }
     
   inline void
   bsd() {
-    std::cout << "Copyright (c) 2019 Dicey Project Authors (see AUTHORS file for details)." << std::endl;
+    std::cout << "Program: Dicey" << std::endl;
+    std::cout << "Version: " << diceyVersionNumber << std::endl;
+    std::cout << "Copyright (C) 2019 European Molecular Biology Laboratory (EMBL) (see LICENSE file for details)." << std::endl;
     std::cout << "All rights reserved." << std::endl;
-    std::cout << std::endl;
-    std::cout << "Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:" << std::endl;
-    std::cout << "    1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer." << std::endl;
-    std::cout << "    2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution." << std::endl;
-    std::cout << "    3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission." << std::endl;
-    std::cout << std::endl;
-    std::cout << "THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, ";
-    std::cout << "THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR ";
-    std::cout << "CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, ";
-    std::cout << "PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, ";
-    std::cout << "WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ";
-    std::cout << "ADVISED OF THE POSSIBILITY OF SUCH DAMAGE." << std::endl;
     std::cout << std::endl;
   }
 
